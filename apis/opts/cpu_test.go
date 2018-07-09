7e9b0d46f9e530374aa6f898a6bab68d47740934
wangyifei24 <40969914+wangyifei24@users.noreply.github.com>
2018-07-09 18:51:56 +0800

    Update cpu_test.go

--- apis/opts/cpu_test.go
+++ apis/opts/cpu_test.go
@@ -1,11 +1,24 @@
 package opts
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestValidateCPUPeriod(t *testing.T) {
-	// TODO
+	cases := []struct {
+          in, want int64
+      }{
+          {0, 0},
+	  {500,0},
+          {1000,0}, 
+      }
+      for _, c := range cases {
+          got := ValidateCPUPeriod(c.in)
+          if got != nil {
+              t.Errorf("CPU cfs period  %d cannot be less than 1ms (i.e. 1000) or larger than 1s (i.e. 1000000)", period)
+          }
+}
 }
 
 func TestValidateCPUQuota(t *testing.T) {
